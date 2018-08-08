4706c2cf0c9842b440d23ff381876a0af78bf714
Pepper Themes <41934799+pepperthemes@users.noreply.github.com>
2018-08-08 15:28:00 +0530

    Add files via upload

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+/*!
+ * Salt v1.0 (https://www.pepperthemes.online)
+ * Copyright AW Pepper Themes
+ * Licensed under MIT
+ */
+
+(function($) {
+  'use strict';
+
+  // Activate Tooltips & Popovers
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+}(jQuery));
